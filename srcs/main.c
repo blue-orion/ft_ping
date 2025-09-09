7248032341d1a43dcc165dade9999aa57b745687
Taeho <xogh080907@gmail.com>
2025-09-10 07:58:31 +0900

    implement icmp protocol simple version, recved reply successful

--- srcs/main.c
+++ srcs/main.c
@@ -12,10 +12,10 @@
 #define ECHO_REQUEST 8
 
 struct icmp_header {
-	uint8_t		icmp_type;
-	uint8_t		icmp_code;
-	uint16_t	icmp_chsum;
-	uint32_t	icmp_payload;
+	uint8_t		type;
+	uint8_t		code;
+	uint16_t	checksum;
+	uint32_t	payload;
 };
 
 int	checksum(void *packet, int len);
@@ -37,25 +37,26 @@ int	main(int ac, char **av) {
 
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
-	if (inet_pton(AF_INET, argv[1], &addr.sin_addr) != 1) {
+	if (inet_pton(AF_INET, av[1], &addr.sin_addr) != 1) {
 		perror("inet_pton");
 		return 1;
 	}
 
 	memset(&header, 0, sizeof(header));
-	header.icmp_type = ECHO_REQUEST;
+	header.type = ECHO_REQUEST;
 
 	memset(packet, 0, sizeof(packet));
 	memcpy(packet, &header, sizeof(header));
-	((struct icmp_header *)packet)->icmp_chsum = checksum(packet, sizeof(packet));
+	((struct icmp_header *)packet)->checksum = checksum(packet, sizeof(packet));
 
-	if (sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr *)&addr, sizeof(addr))) {
+	if (sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
 		perror("sendto");
 		return 1;
 	}
 
 	char	result[PACKET_SIZE];
-	if (recvfrom(sockfd, result, sizeof(result), 0, (struct sockaddr *)&addr, (socklen_t *)sizeof(addr))) {
+	socklen_t	len = sizeof(addr);
+	if (recvfrom(sockfd, result, sizeof(result), 0, (struct sockaddr *)&addr, &len) == -1) {
 		perror("recvfrom");
 		return 1;
 	}
